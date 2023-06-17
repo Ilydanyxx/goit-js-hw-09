b242d637525ba7a055e904e341dadee8ba6589e2
Ilydanyxx <danya.chuyko@icloud.com>
2023-06-17 10:54:16 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
-
+const start = document.querySelector("button[data-start]");
+const stop = document.querySelector("button[data-stop]");
+const body = document.querySelector("body")
+start.addEventListener("click", () => {
+    timer = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    start.setAttribute("disabled", "")
+})
+stop.addEventListener("click", () => {
+    clearInterval(timer);
+    start.removeAttribute("disabled")
+})
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+   return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  
 }
+
+const form = document.querySelector("form");
+const delay = Number(document.querySelector('input[name="delay"]').value);
+const step = Number(document.querySelector('input[name="step"]').value);
+const amount = Number(document.querySelector('input[name="amount"]').value);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  for (let i = 0; i < amount; i++) {
+  createPromise(i, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+  delay += step;
+}
+
+})
+
+
+
+
+
