17c58f68faf82b834e3e5a399dc6b2028e28aec0
Ilydanyxx <122315740+Ilydanyxx@users.noreply.github.com>
2023-06-19 00:22:58 +0000

    Deploying to gh-pages from @ Ilydanyxx/goit-js-hw-09@38e8cbf7e67d658595d5dd6f2b846be742d5b4e9 🚀

---
+++ 01-color-switcher.aeee8c4d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){timer=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled","disabled")})),e.addEventListener("click",(function(){clearInterval(timer),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.aeee8c4d.js.map
